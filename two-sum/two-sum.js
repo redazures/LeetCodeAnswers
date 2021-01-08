735ab59849c4c05d84903bc438f278b5f8d403a0
James Wu <65046903+redazures@users.noreply.github.com>
2021-01-08 15:09:57 -0500

    Time: 92 ms (26.28%), Space: 39 MB (33.26%) - LeetHub

--- two-sum/two-sum.js
+++ two-sum/two-sum.js
@@ -4,12 +4,9 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-        var key = {}
+    var key = {}
     for(let i=0;i<nums.length;i++){
-        key[target-nums[i]]=i
+        if (nums[i] in key && key[nums[i]] !== i) return [key[nums[i]],i]
+        key[target-nums[i]]=i     
     }
-    
-    for(let j=0;j<nums.length;j++){
-        if (key[nums[j]] && j!==key[nums[j]]) return [j,key[nums[j]]]
-    }
 };
