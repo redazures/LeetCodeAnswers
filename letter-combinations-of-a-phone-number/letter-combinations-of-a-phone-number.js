a1a1d782950c85b8648eec52ad95e9e44b275c3e
James Wu <65046903+redazures@users.noreply.github.com>
2021-02-07 13:23:44 -0500

    Time: 72 ms (91.15%), Space: 38.7 MB (38.48%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -3,8 +3,8 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-    if(!digits.length) return [];
-    key={ "1":[],
+        if(!digits.length) return [];
+        key={ "1":[],
          "2":["a","b","c"],
          "3":["d","e","f"],
          "4":["g","h","i"],
@@ -13,25 +13,21 @@ var letterCombinations = function(digits) {
          "7":["p","q","r","s"],
          "8":["t","u","v"],
          "9":["w","x","y","z"],
-    }
+        }
     
+    let answer = [...key[digits[0]]]
     
-    let result = [...key[digits[0]]]
-    
-    for(let i = 1; i < digits.length; i++) {
+    for(let i=1; i<digits.length;i++){
         const digit = digits[i]
         const current = key[digit]
         const temp = []
-        for(let j = 0; j < result.length; j++) {
-            for(let k = 0; k < current.length; k++) {
-                // console.log(result, temp, current)
-                temp.push(result[j] + current[k])
-                // console.log(result[j],current[k])
+        for(let j=0;j<answer.length;j++){
+            for(let k=0;k<current.length;k++){
+                temp.push(answer[j]+current[k])
             }
-            // console.log("end")
         }
-        result = temp
+        answer = temp
     }
-    return result
+    return answer
     
 };
