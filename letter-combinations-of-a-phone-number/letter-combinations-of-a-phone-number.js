cfe476ddeec77d4f498ea0961679a209e230a1e2
James Wu <65046903+redazures@users.noreply.github.com>
2021-02-07 14:38:25 -0500

    Time: 76 ms (74.02%), Space: 38.7 MB (38.61%) - LeetHub

--- letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
+++ letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.js
@@ -3,31 +3,31 @@
  * @return {string[]}
  */
 var letterCombinations = function(digits) {
-        if(!digits.length) return [];
-        key={ "1":[],
-         "2":["a","b","c"],
-         "3":["d","e","f"],
-         "4":["g","h","i"],
-         "5":["j","k","l"],
-         "6":["m","n","o"],
-         "7":["p","q","r","s"],
-         "8":["t","u","v"],
-         "9":["w","x","y","z"],
-        }
+    if(digits==="")return[]
+    key={ "1":[],
+     "2":["a","b","c"],
+     "3":["d","e","f"],
+     "4":["g","h","i"],
+     "5":["j","k","l"],
+     "6":["m","n","o"],
+     "7":["p","q","r","s"],
+     "8":["t","u","v"],
+     "9":["w","x","y","z"],
+    }
     
-    let answer = [...key[digits[0]]]
+    let results = [...key[digits[0]]]
     
-    for(let i=1; i<digits.length;i++){
-        const digit = digits[i]
-        const current = key[digit]
-        const temp = []
-        for(let j=0;j<answer.length;j++){
-            for(let k=0;k<current.length;k++){
-                temp.push(answer[j]+current[k])
+    
+    for(let i=1;i<digits.length;i++){
+        const num = digits[i]
+        const letters = key[num]
+        let temp =[]
+        for(let j=0;j<results.length;j++){
+            for(let k=0;k<letters.length;k++){
+                temp.push(results[j]+letters[k])
             }
         }
-        answer = temp
+        results=temp
     }
-    return answer
-    
+    return results
 };
