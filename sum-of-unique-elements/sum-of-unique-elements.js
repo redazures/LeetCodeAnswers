0798cdf3ac3e4681b3498196cd6c62b5e16c0216
James Wu <65046903+redazures@users.noreply.github.com>
2021-02-08 14:56:54 -0500

    Time: 76 ms (89.23%), Space: 38.7 MB (59.23%) - LeetHub

--- sum-of-unique-elements/sum-of-unique-elements.js
+++ sum-of-unique-elements/sum-of-unique-elements.js
@@ -3,21 +3,34 @@
  * @return {number}
  */
 var sumOfUnique = function(nums) {
-    let keys={}
-    for(let i=0;i<nums.length;i++){
-        const num = nums[i]
-        if (keys[num]) {keys[num]++}
-        else{keys[num] = 1}
-    }
-    console.log(keys)
-    let sum = 0
+//     let keys={}
+//     let sum = 0
+//     for(let i=0;i<nums.length;i++){
+//         const num = nums[i]
+//         if (!keys[num]) {
+//             keys[num] = 1
+//             sum+=num
+//         }
+//         else {
+//             keys[num]++
+//             if(keys[num]===2)sum-=num
+//         }
+//     }
     
-    for(let num in keys){
-        console.log(keys[num]===1,"this is")
-        // console.log(parseInt(num)+)
-        if(keys[num]===1)sum=sum + parseInt(num)
-    }
-    // console.log(sum)
-    // arr.reduce((sum,v)=>sum+v,0)
-    return sum
+//     // for(let num in keys){
+//     //     if(keys[num]===1)sum=sum + parseInt(num)
+//     // }
+//     return sum
+    
+           let map = new Object();
+    nums.forEach(num => {
+        if(map[num] != null) map[num] += 1;
+        else map[num] = 1;
+    })
+    let sum = 0;
+    Object.keys(map).forEach(k => {
+        if(map[k] === 1)   sum += +k
+    })
+    return sum;
+    
 };
