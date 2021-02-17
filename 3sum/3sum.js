5c491256c3705587ae0bb1c13f3520ddaf97866c
James Wu <65046903+redazures@users.noreply.github.com>
2021-02-16 20:17:48 -0500

    Time: 152 ms (70.17%), Space: 49.1 MB (47.29%) - LeetHub

--- 3sum/3sum.js
+++ 3sum/3sum.js
@@ -4,28 +4,26 @@
  */
 var threeSum = function(nums) {
     nums.sort((a,b)=>a-b)
-    let solutions = []
+    let res = []
     
-    for(let i =0; i<nums.length-2;i++){
-        if(nums[i]!=nums[i-1]){
-            let left =i+1
-            let right=nums.length-1
-            while(left<right){
-                const sum = nums[i]+nums[left]+nums[right]
-                if(sum===0){
-                    solutions.push([nums[i],nums[left],nums[right]])
-                    while(nums[left] == nums[left+1]) left++
-                    while(nums[right] == nums[right+1]) right--
-                    left++
-                    right--
-                }else if(sum<0){
-                    left++
-                }else if(sum>0){
-                    right--
-                }
+    for(let i=0 ; i<nums.length ; i++){
+        if(nums[i]===nums[i-1]) continue
+        let left = i+1, right=nums.length-1
+        while(left<right){
+            const sum = nums[i]+nums[left]+nums[right]
+            // console.log(nums[i],nums[left],nums[right],sum,res)
+            if(sum===0){
+                res.push([nums[i],nums[left],nums[right]])
+                while(nums[left]===nums[left+1])left++
+                while(nums[right]===nums[right+1])right--
+                left++
+                right--
+            }else if(sum>0){
+                right--
+            }else if (sum<0) {
+                left++
             }
         }
     }
-    
-    return solutions
+    return res
 };
