6d3faebfc500a47628c75c3e02b26932120ddf91
James Wu <65046903+redazures@users.noreply.github.com>
2021-02-18 22:33:01 -0500

    Time: 108 ms (97.14%), Space: 50.3 MB (33.07%) - LeetHub

--- product-of-array-except-self/product-of-array-except-self.js
+++ product-of-array-except-self/product-of-array-except-self.js
@@ -3,18 +3,14 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-    let arr=[], turn=false, mutiple=1
+    let arr=[], mutiple=1
     for(let i=0; i<nums.length;i++){
-        // console.log("works")
         arr[i]=mutiple
         mutiple*=nums[i]
     }
     
-    console.log(arr)
-    
     mutiple = 1 
     for(let j=nums.length-1; j>=0;j--){
-        console.log(mutiple,nums[j],arr[j])
         arr[j]*=mutiple
         mutiple*=nums[j]
     }
