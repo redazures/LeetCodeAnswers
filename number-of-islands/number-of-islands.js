330784ba15e06246fd5d13be3a4d12813af1059a
James Wu <65046903+redazures@users.noreply.github.com>
2021-02-27 20:10:29 -0800

    Time: 104 ms (27.88%), Space: 41.1 MB (40.47%) - LeetHub

--- number-of-islands/number-of-islands.js
+++ number-of-islands/number-of-islands.js
@@ -3,31 +3,31 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    let count =0
-    
-    function connecting(x,y){
-        let stack =[[x,y]]
-        
-        while(stack.length){
-            const [x, y] = stack.pop()
-            if(grid[x][y]==="1")grid[x][y]="2"
-            // console.log(grid[x+1],grid[x][y+1],)
-            if(grid[x+1] && grid[x+1][y]==="1" )stack.push([x+1,y])
-            if(grid[x-1] && grid[x-1][y]==="1")stack.push([x-1,y])
-            if(grid[x][y+1] && grid[x][y+1]==="1")stack.push([x,y+1])
-            if(grid[x][y-1] && grid[x][y-1]==="1")stack.push([x,y-1])
-            // console.log(x,y,stack)
-        }    
+  
+    const validateIsland =  (x,y,grid) =>{
+        let stack = [[x,y]]
+        while(stack.length>0){
+            let [ a, b ] = stack.pop()
+            grid[a][b]="J"
+            if(grid[a+1] && grid[a+1][b]==="1")stack.push([a+1,b])
+            if(grid[a-1] && grid[a-1][b]==="1")stack.push([a-1,b])
+            if(grid[a][b+1] && grid[a][b+1]==="1")stack.push([a,b+1])
+            if(grid[a][b-1] && grid[a][b-1]==="1")stack.push([a,b-1])
+        }
     }
     
+    // validateIsland(0,0,grid)
+    let counter = 0 
     for(let i=0;i<grid.length;i++){
         for(let j=0;j<grid[i].length;j++){
+            // console.log(grid[i][j])
+            // console.log("what")
             if(grid[i][j]==="1"){
-                connecting(i,j)
-                count++
+                validateIsland(i,j,grid)
+                counter++
+                // console.log("checking")
             }
         }
     }
-    console.log(grid)
-    return count
+    return counter
 };
