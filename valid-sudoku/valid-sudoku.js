5e1f3810c0045f4504580e30128b8bf386be5286
James Wu <65046903+redazures@users.noreply.github.com>
2021-08-02 09:47:03 -0400

    Time: 92 ms (73.37%), Space: 43 MB (37.83%) - LeetHub

--- valid-sudoku/valid-sudoku.js
+++ valid-sudoku/valid-sudoku.js
@@ -3,45 +3,24 @@
  * @return {boolean}
  */
 var isValidSudoku = function(board) {
-    let validate={'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
-    
-    //row
+    let validate = new Set()
     for(let i=0;i<board.length;i++){
-        for(let k=0;k<board[i].length;k++){
-            const slot = board[i][k]
-            validate[slot]++
-            if(validate[slot]>1)return false
-        }
-        validate={'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
-    }
-    //column
-    for(let i=0;i<board.length;i++){
-        for(let k=0;k<board[i].length;k++){
-            const slot = board[k][i]
-            validate[slot]++
-            if(validate[slot]>1)return false
-        }
-        validate={'1': 0, '2': 0, '3': 0, '4': 0, '5': 0, '6': 0, '7': 0, '8': 0, '9': 0}
-    }
-    
-    for (let i = 0; i < board.length; i = i + 3){
-        for (let j = 0; j < board[0].length; j = j + 3) {
-            let matrixMap = {};
-            let subX = i + 3;
-            let subY = j + 3;
-            for (let k = i; k < subX; k++) {
-                for (let l = j; l < subY; l++) {
-                    let currentValue = board[k][l];
-                    if (currentValue === '.') continue;
-                    if (matrixMap[currentValue] === undefined) {
-                        matrixMap[currentValue] = currentValue;
-                    } else {
-                        return false;
-                    }
+        for(let j=0; j<board.length;j++){
+            const slot = board[i][j]
+            if(slot!="."){
+                const row = `row ${i}-num ${slot}`
+                const column = `col ${j}-num ${slot}`
+                const innerBox = `innerBox ${parseInt(i/3)} ${parseInt(j/3)} ${slot}`
+                if(!validate.has(row) && !validate.has(column) && !validate.has(innerBox)){
+                    validate.add(row)
+                    validate.add(column)
+                    validate.add(innerBox)
+                }else{
+                    return false
                 }
             }
         }
     }
-    
     return true
+    
 };
