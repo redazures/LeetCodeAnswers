7cfa55a55255c597696832c27ff719a8206da3eb
James Wu <65046903+redazures@users.noreply.github.com>
2021-08-02 21:56:37 -0400

    Time: 92 ms (94.88%), Space: 40.7 MB (97.82%) - LeetHub

--- longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
+++ longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.js
@@ -3,12 +3,12 @@
  * @return {number}
  */
 var lengthOfLongestSubstring = function(s) {
-    let maxLen=0, start = 0, map = new Map()
-    for(let i=0; i<s.length; i++){
+    let maxLen = 0, map = new Map(), start = 0
+    for(let i=0; i<s.length;i++){
         const ch = s[i]
-        if(map.get(ch)>=start)start = map.get(ch)+1
+        if(map.get(ch)>=start)start=map.get(ch)+1
         map.set(ch, i)
-      if(i - start + 1 > maxLen) maxLen = i - start + 1;
+        maxLen=Math.max(i-start+1, maxLen)
     }
     return maxLen
 };
