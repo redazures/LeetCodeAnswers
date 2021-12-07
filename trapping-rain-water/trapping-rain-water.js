fc8478113a6897d321f415d4cad8b7ea945d5002
James Wu <65046903+redazures@users.noreply.github.com>
2021-12-07 00:53:50 -0500

    Time: 145 ms (14.24%), Space: 40.6 MB (86.43%) - LeetHub

--- trapping-rain-water/trapping-rain-water.js
+++ trapping-rain-water/trapping-rain-water.js
@@ -3,33 +3,24 @@
  * @return {number}
  */
 var trap = function(height) {
-    if(height.length<=2) return 0
+  let l = 0;
+  let r = height.length - 1;
+  let lvl = 0;
+  let sum = 0;
 
-    let start, end, sum=0, max=0, index=0
-    
-    for(let k=0;k<height.length;k++){
-        const prev = max
-        max=Math.max(max,height[k])
-        if(prev!==max)index=k
+  while (l < r) {
+    let lower = 0;
+    if (height[l] < height[r]) {
+      lower = height[l];
+      l++;
+    } else {
+      lower = height[r];
+      r--;
     }
+    lvl = Math.max(lower, lvl);
+    sum += lvl - lower;
+  }
+  
+  return sum;
     
-    for(let i=0;i<index;i++){
-        if(!start || height[i]>start){
-            start=height[i]
-        } else {
-            const difference = (start-height[i])
-            sum = sum + difference
-        }
-    }
-    
-    for(let j=height.length-1;j>index;j--){
-        if(!end || height[j]>end){
-            end=height[j]
-        }
-        else{
-            sum+=(end-height[j])
-        }
-    }
-    
-    return sum
 };
