056226b3bc171e1729a69e0d1b83e8da78d10900
James Wu <65046903+redazures@users.noreply.github.com>
2022-01-25 03:02:08 -0500

    Time: 123 ms (41.85%), Space: 49.2 MB (15.12%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function(nums) {
+    let sum = 0, highest = Number.MIN_SAFE_INTEGER
+
+    for(let i=0;i<nums.length;i++){
+        const num = nums[i]
+        sum+=num
+        highest = Math.max(highest, sum)
+        if(sum<0){
+            sum = 0
+        }
+    }
+    
+    return highest
+};
