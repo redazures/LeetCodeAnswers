a65f948f1e8f2505f05d7e2bb097a5623f0de42b
James Wu <65046903+redazures@users.noreply.github.com>
2022-01-25 11:38:57 -0500

    Time: 92 ms (80.00%), Space: 44 MB (52.93%) - LeetHub

---
+++ 227-basic-calculator-ii/227-basic-calculator-ii.js
@@ -0,0 +1,39 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+    const special = {"*":1, "/":2," ":5,"-":3, "+":4}
+    s=s+"+"
+    let stack = [], sign=undefined, number=""
+    for(let i=0; i<s.length; i++){
+        const char = s[i]
+        if(special[char]===5) continue
+        if(special[char] && special[char]<5){
+            let int = Number(number)
+            if(sign==="-"){
+                int = -int
+            }
+            if(sign==="*"){
+                const last  = stack.pop()
+                int = int*last
+            }
+            if(sign==="/"){
+                const last  = stack.pop()
+                if(last<0){
+                    int = Math.ceil(last/int)
+                }else{
+                    int = Math.floor(last/int)   
+                }
+            }
+            stack.push(int)
+            sign = char
+            number=""
+        }else{
+            number+=char
+        }
+        sign
+    }
+    console.log(stack)
+    return stack.reduce((a,b)=>a+b)
+};
