f445585646c0baa723b8e594c6d73a1f5316825b
James Wu <65046903+redazures@users.noreply.github.com>
2022-01-25 03:06:41 -0500

    Time: 119 ms (44.07%), Space: 48.3 MB (91.98%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -4,13 +4,10 @@
  */
 var maxSubArray = function(nums) {
     let sum = 0, highest = Number.MIN_SAFE_INTEGER
-
     for(let i=0;i<nums.length;i++){
         sum+=nums[i]
         highest = Math.max(highest, sum)
-        if(sum<0){
-            sum = 0
-        }
+        sum = sum<0 ? 0 : sum
     }
     
     return highest
