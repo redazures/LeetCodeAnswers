558994953f62a562cf4fbaae589e93353da60707
James Wu <65046903+redazures@users.noreply.github.com>
2022-03-27 13:07:44 -0400

    Time: 930 ms (28.53%), Space: 153 MB (9.03%) - LeetHub

---
+++ 204-count-primes/204-count-primes.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countPrimes = function(n) {
+    let nums = [];
+    let primeCount = 0;
+    for(let i =0; i < n; i++){
+        nums[i] = true;
+    }
+    for(let i=2; i * i < n; i++){
+        if (nums[i] == true){
+            for(let j = 2; j* i <n;j++){
+                nums[j* i] = false;
+            }
+        }
+    }
+    
+    for (let i = 2; i < n; i++){
+        if (nums[i] === true){
+            primeCount++;
+        }
+    }
+    return primeCount;
+};
