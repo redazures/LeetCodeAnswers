7e1704151f226657a219f0e5f45aeba734ef088e
James Wu <65046903+redazures@users.noreply.github.com>
2022-03-27 02:29:46 -0400

    Time: 100 ms (55.74%), Space: 43.7 MB (94.30%) - LeetHub

---
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    let newString = ""
+    
+    newString= s.replace(/[^A-Z0-9]+/ig, "").toLowerCase()
+    
+    let start = 0 
+    let end = newString.length-1
+    while(start<end){
+        if(newString[start]!=newString[end])return false
+        start++
+        end--
+    }
+    return true
+};
