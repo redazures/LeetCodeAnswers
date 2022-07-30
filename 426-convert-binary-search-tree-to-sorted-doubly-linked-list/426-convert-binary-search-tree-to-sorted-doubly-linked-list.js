45b610d68b7085611502e390dd5750524ab637cc
James Wu <65046903+redazures@users.noreply.github.com>
2022-07-30 00:07:22 -0400

    Time: 66 ms (93.51%), Space: 44.4 MB (27.92%) - LeetHub

---
+++ 426-convert-binary-search-tree-to-sorted-doubly-linked-list/426-convert-binary-search-tree-to-sorted-doubly-linked-list.js
@@ -0,0 +1,34 @@
+/**
+ * // Definition for a Node.
+ * function Node(val, left, right) {
+ *      this.val = val;
+ *      this.left = left;
+ *      this.right = right;
+ *  };
+ */
+
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var treeToDoublyList = function(root) {
+    let tail, head
+    convertNode = (node) => {
+        if(!node) return
+        convertNode(node.left)
+        if(!head) head = node
+        if(tail) {
+            tail.right = node
+        }
+        node.left = tail
+        tail = node
+        convertNode(node.right)
+    }
+    convertNode(root)
+    
+    if(head){
+        head.left = tail
+        tail.right = head
+    }
+    return head
+};
