2812afa2dedb9b7a5ab0ab9916740e000cfa6208
James Wu <65046903+redazures@users.noreply.github.com>
2022-08-07 14:13:37 -0400

    Time: 111 ms (37.67%), Space: 44.4 MB (38.71%) - LeetHub

---
+++ 496-next-greater-element-i/496-next-greater-element-i.js
@@ -0,0 +1,23 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var nextGreaterElement = function(nums1, nums2) {
+    let index = {}, res = []
+    nums2.forEach((el,idx)=>{
+        index[el] = -1
+        for(let i=idx;i<nums2.length;i++){
+            const num = nums2[i]
+            if(el<num && index[el]===-1){
+                index[el] = num
+            }
+        }        
+    })
+    // console.log(index)
+    nums1.forEach((el)=>{
+        const idx = index[el]
+        res.push(idx)
+    })
+    return res
+};
